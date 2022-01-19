7900b3c802664cf35db3b5d015f603a043cd6238
Josephvs96 <bahhadi.joseph@gmail.com>
2022-01-19 11:32:04 +0100

    Added two projects for the creation of the Azure index and fraud detection console applications

---
+++ DotNetBanky.SearchIndexCreator/Program.cs
@@ -0,0 +1,28 @@
+﻿using DotNetBanky.BLL.Services;
+using DotNetBanky.Common.DIContainer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder();
+
+host.ConfigureDefaults(args);
+
+host.ConfigureServices((context, services) =>
+{
+    services.AddBankyDatabase(context.Configuration);
+    services.AddBankyServices(context.Configuration);
+    services.AddAzureSearch(context.Configuration);
+});
+
+var app = host.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var searchService = scope.ServiceProvider.GetRequiredService<ISearchService>();
+
+    await searchService.CreateAndPopulateIndex();
+}
+
+app.Run();
+
+
