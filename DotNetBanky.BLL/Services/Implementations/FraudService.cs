bab77aaa70553523262be88e77e7b50d950a1ebd
Joseph bahhadi <josephbhd95@outlook.com>
2022-01-22 22:22:54 +0100

    Optimized the fraud detections algorithm

--- DotNetBanky.BLL/Services/Implementations/FraudService.cs
+++ DotNetBanky.BLL/Services/Implementations/FraudService.cs
@@ -1,5 +1,6 @@
 ﻿using DotNetBanky.Core.Constants;
 using DotNetBanky.Core.DTOModels.Fraud;
+using DotNetBanky.Core.DTOModels.Paging;
 using DotNetBanky.Core.Entities;
 using DotNetBanky.DAL.Repositories.IRepositories;
 using Microsoft.EntityFrameworkCore;
@@ -27,7 +28,80 @@ namespace DotNetBanky.BLL.Services.Implementations
 
             foreach (var country in countries)
             {
-                await ProccessCountry(country, lastScannedDate);
+                //await ProccessCountry(country, lastScannedDate);
+                await ProccessCountryInBatches(country, lastScannedDate);
+            }
+        }
+
+        private async Task ProccessCountryInBatches(string country, DateTime lastScannedDate)
+        {
+            var fraudList = new List<FraudDTO>();
+            const int batchSize = 3000;
+            int currentBatch = 1;
+
+            while (true)
+            {
+                var batch = await _customerRepository.GetPagedListAsync(
+                filter: c => c.Country == country,
+                include: q => q.Include(c => c.Dispositions).ThenInclude(d => d.Account),
+                page: currentBatch, pageSize: batchSize);
+
+                foreach (var customer in batch.Results)
+                {
+                    foreach (var account in customer.Dispositions)
+                    {
+                        var currentTransactionsBatch = 1;
+                        while (true)
+                        {
+                            var accountTransactions = await _transactionRepository.GetPagedListAsync(
+                                filter: t => t.AccountNavigation.AccountId == account.Account.AccountId && t.Date > lastScannedDate,
+                                orderBy: q => q.OrderBy(t => t.Date),
+                                page: currentTransactionsBatch, pageSize: batchSize);
+
+                            foreach (var transaction in accountTransactions.Results)
+                            {
+                                ProccessTransaction(fraudList, customer, account, accountTransactions, transaction);
+                            }
+
+                            currentTransactionsBatch++;
+                            if (currentTransactionsBatch > batch.PageCount) break;
+                        }
+                    }
+                }
+
+                currentBatch++;
+                if (currentBatch > batch.PageCount) break;
+            }
+
+            if (fraudList.Count <= 0) return;
+
+            SendListToCountryMail(country, fraudList);
+        }
+
+        private static void ProccessTransaction(List<FraudDTO> fraudList, Customer customer, Disposition account, PagedResult<Transaction> accountTransactions, Transaction transaction)
+        {
+            var transactionsNextThreeDays = accountTransactions.Results.Where(
+                    t => t.Date >= transaction.Date &&
+                    t.Date <= transaction.Date.AddHours(72));
+
+            if (IsFraudTransaction(fraudList, transactionsNextThreeDays))
+            {
+                fraudList.AddRange(transactionsNextThreeDays.Select(t => new FraudDTO
+                {
+                    AccountId = account.Account.AccountId,
+                    CustomerId = customer.CustomerId,
+                    TransactionId = t.TransactionId
+                }));
+            }
+
+            if (IsFraudTransaction(fraudList, transaction))
+            {
+                fraudList.Add(new FraudDTO
+                {
+                    TransactionId = transaction.TransactionId,
+                    CustomerId = customer.CustomerId,
+                    AccountId = account.Account.AccountId,
+                });
             }
         }
 
